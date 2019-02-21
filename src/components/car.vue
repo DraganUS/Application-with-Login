701a045741a4039e2e5db57a9fa31ab353b2e21d
DraganUS <usic.d.dragan@gmail.com>
2019-02-21 16:16:10 +0100

    axios api added

---
+++ src/components/car.vue
@@ -0,0 +1,39 @@
+<template>
+<div id='cards'>
+
+    <div class="" v-for="(value, index) in info.data" >
+                 <p>{{ value.title }}</p>
+                 <img v-bind:src="value.img" alt="" >
+                 <p>{{ value.description }}</p>
+              </div>
+
+</div>
+</template>
+  <script>
+export default {
+  data(){
+    return {
+        info: ''
+    }
+  },
+  mounted(){
+    axios
+    .get('https://api.myjson.com/bins/15xkc6')
+    .then(response => (this.info = response))   
+
+    
+  }
+  
+}
+</script>
+
+<style scoped>
+div#cards{
+  height: 500px;
+  width: 100%;
+  background-color: bisque;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+</style>
--- src/components/home.vue
+++ src/components/home.vue
@@ -1,15 +1,18 @@
 <template>
 	<div class="home" >
 		<navMenu/>
+		<car/>
 	</div>
 </template>
 
 <script>
 import navMenu from "@/components/navMenu.vue";
+import car from "@/components/car.vue";
 export default {
 	name: 'home',
 	components:{
-		navMenu
+		navMenu,
+		car
 	},
 	data() {
 		return {
